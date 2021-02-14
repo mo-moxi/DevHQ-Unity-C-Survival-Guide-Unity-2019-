a5290b1dfd77dc3d559652888d52240d806a1208
mo-moxi <mem53211@gmail.com>
2021-02-14 21:53:43 +0000

    Dictionaries

---
+++ Assets/Scripts/ItemDB.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDB : MonoBehaviour
+{
+    public List<Item> itemList = new List<Item>();
+    
+    public Dictionary<int, Item> itemDictionary = new Dictionary<int, Item>();
+    
+    void Start()
+    {
+        itemDictionary.Add(0, itemList[0]);
+
+        var item = itemDictionary[0];
+        Debug.Log("Item at key 0 is: "+ item.name);
+    }
+    
+    void Update()
+    {
+        
+    }
+}
