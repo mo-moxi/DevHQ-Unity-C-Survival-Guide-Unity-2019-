dcb565a4a0c2d731ea90cd19baa9d4c574ceb00e
mo-moxi <mem53211@gmail.com>
2021-02-14 00:59:42 +0000

    Challenge Review You Have Been Named

---
+++ Assets/Scripts/Named_List.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Named_List : MonoBehaviour
+{
+    [SerializeField]
+    private List<string> names = new List<string>();
+    private void Start()
+    {
+        PrintNames();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var i = Random.Range(0, names.Count);
+            if (names.Count > 0)
+            {
+                RemoveName(i);
+                PrintNames();
+            }
+        }
+    }
+    private void PrintNames()
+    {
+        foreach (var item in names)
+        {
+            Debug.Log("Name: " + item);
+        }
+    }
+    private void RemoveName(int i)
+    {
+        Debug.Log("We removed: " + names[i]);
+        names.Remove(names[i]);
+    }
+}
