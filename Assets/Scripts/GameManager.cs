a002a07638d616d02426fe2032f0e99b1aca9b56
mo-moxi <mem53211@gmail.com>
2021-02-14 01:16:37 +0000

    Challenge Build That Object List

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    // Challenge: Create an array of three game object and randomly
+    // spawn them between +/-10 on x/y when space key is pressed.
+    // limit spawn to ten then turn spawned object green before 
+    // clearing the list.
+    [SerializeField] private List<GameObject> itemsToSpawn = new List<GameObject>();
     private void Start()
     {
 
     }
     private void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // spawn objects
+            // increment counter to 10
+            // on eleven turn them green
+            // on 12 clear the list
+        }
     }
-
 }
