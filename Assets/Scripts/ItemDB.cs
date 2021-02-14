d51ac5d9ae14248def535be983c6bdccf5f295d0
mo-moxi <mem53211@gmail.com>
2021-02-14 23:09:53 +0000

    Dictionaries

--- Assets/Scripts/ItemDB.cs
+++ Assets/Scripts/ItemDB.cs
@@ -10,14 +10,47 @@ public class ItemDB : MonoBehaviour
     
     void Start()
     {
-        itemDictionary.Add(0, itemList[0]);
-
-        var item = itemDictionary[0];
-        Debug.Log("Item at key 0 is: "+ item.name);
+        for (int i = 0; i < itemList.Count; i++)
+        {
+            itemDictionary.Add(i, itemList[i]);
+        }
     }
-    
     void Update()
     {
-        
+        // always check that key exists before access 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var i = Random.Range(0, itemDictionary.Count);
+            var item = itemDictionary[i];
+            Debug.Log("Item at key is " + i +" and item is "+ item.name);
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            foreach(KeyValuePair<int, Item> item in itemDictionary)
+                Debug.Log("Item at key " + item.Key +" is: "+ item.Value.name);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            foreach(var key in itemDictionary.Keys)
+                Debug.Log("Item at keys " + key);
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            foreach(var item in itemDictionary.Values)
+                Debug.Log("Item at keys " + item.name);
+        }
+        // Assign random found item
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (itemDictionary.ContainsKey(5))
+            {
+                var randomItem = itemDictionary[5];
+                Debug.Log("You found key ");
+            }
+            else
+            {
+                Debug.Log("Key does not exist");
+            }
+        }
     }
 }
