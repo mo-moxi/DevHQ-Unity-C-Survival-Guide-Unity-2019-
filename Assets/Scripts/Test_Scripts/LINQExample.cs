5a1214d9c52d4c255cba3b12000da54c21fe1827
Mo Moxi <mem53211@gmail.com>
2021-02-16 18:42:04 +0000

    LINQ distinct

--- Assets/Scripts/Test_Scripts/LINQExample.cs
+++ Assets/Scripts/Test_Scripts/LINQExample.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 using System.Linq;
 public class LINQExample : MonoBehaviour
 {
-    public string[] names = {"jon", "gillian", "simon"};
+    public string[] names = {"jon", "gillian", "jon", "simon", "gillian", "Wu"};
     // Start is called before the first frame update
     void Start()
     {
-    /*    foreach (var item in names)
-        {
-            if(item == "simon")
-                Debug.Log("Name exists: " + name);
-        }
+    /*  // traditional method
+        foreach (var item in names)
+        {       if(item == "simon")
+                Debug.Log("Name exists: " + name);}
     */
     // var namesFound = names.Any(name => name == "simon");
     var namesFound = names.Contains("gillian");
     Debug.Log("Does name exist:" + namesFound);
+    var uniqueNames = names.Distinct();
+    foreach (var item in uniqueNames)
+    {
+        Debug.Log("Name: " + item);
+    }
+    
     }
 
     // Update is called once per frame
