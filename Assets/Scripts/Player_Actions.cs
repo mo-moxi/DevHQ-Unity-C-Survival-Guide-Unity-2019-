ef247f3df68f06663b469f6256264c1b240c2e58
Mo Moxi <mem53211@gmail.com>
2021-02-16 15:20:42 +0000

    Actions

---
+++ Assets/Scripts/Player_Actions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class Player_Actions : MonoBehaviour
+{
+    // traditional delegate (version 2)
+    //public delegate void OnDamageReceived(int currentHealth);
+    //public static event OnDamageReceived onDamage;
+
+    // Version 3 and above use Action
+    
+    public static Action<int> OnDamagedReceived;   
+    public int Health { get; set; }
+
+    void Start()
+    {
+        Health = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Damage();
+        }
+            
+    }
+
+    public void Damage()
+    {
+        Health--;
+        if (OnDamagedReceived != null)
+            OnDamagedReceived(Health);
+    }
+}
---
+++ Assets/Scripts/UIManager_Actions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager_Actions : MonoBehaviour
+{
+    public Text healthText;
+    public void OnEnable()
+    {
+        Player_Actions.OnDamagedReceived += UpDateHealth;
+    }
+
+    public void UpDateHealth(int health)
+    {
+        healthText.text = "Health: " + health;
+        // display updated health
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
