d73eba508f2cfb852b2c2f383ce13aae24813ec6
Mo Moxi <mem53211@gmail.com>
2021-02-16 20:40:12 +0000

    Challenge review - filter items

--- Assets/Scripts/Test_Scripts/LINQ_Filter_Items.cs
+++ Assets/Scripts/Test_Scripts/LINQ_Filter_Items.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [System.Serializable]
 public class Items
@@ -14,11 +15,24 @@ public class Items
 
 public class LINQ_Filter_Items : MonoBehaviour
 {
-    public List<Items> itemsList;
+    public List<Items> items;
     // Start is called before the first frame update
     void Start()
     {
-        var namesFound = itemsList.Contains("3");
+      var result = items.Any(items => items.itemID == 3);
+      Debug.Log("Result:" + result);
+
+      var buffResult = items.Where(items => items.buff > 20);
+      foreach (var item in buffResult)
+      {
+          Debug.Log("Name Result:" + item.name);
+      }
+
+      var buffAvg = items.Average(item => item.buff);
+      Debug.Log("Buff avg:" + buffAvg);
+      
+
+
     }
 
     // Update is called once per frame
