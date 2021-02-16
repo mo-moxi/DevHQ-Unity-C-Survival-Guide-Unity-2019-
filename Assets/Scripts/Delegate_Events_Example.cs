64d9ed1d518040d4e0acc528365d3b6d65c23860
Mo Moxi <mem53211@gmail.com>
2021-02-16 11:50:36 +0000

    Delegate and events example

---
+++ Assets/Scripts/Delegate_Events_Example.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delegate_Events_Example : MonoBehaviour
+{
+    public delegate void ActionClick();
+
+    public static event ActionClick onClick;
+    
+    public void ButtonClick()
+    {
+        Debug.Log("Button");
+        // turn all cubes green
+        if(onClick !=null)
+            onClick();
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Event_Sphere.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event_Sphere : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Events_1.onClick += Fall;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Fall()
+    {
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+}
