094e5e901692c9235a58c3461e6db04b5f74da7e
Mo Moxi <mem53211@gmail.com>
2021-02-16 17:50:06 +0000

    Simple Callback System

---
+++ Assets/Scripts/Test_Scripts/LINQ.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LINQ : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(MyRoutine(() =>
+        {
+            Debug.Log("Shit happens!");
+        }));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator MyRoutine(Action onComplete = null)
+    {
+        yield return new WaitForSeconds(5.0f);
+
+        if (onComplete != null)
+            onComplete();
+    }
+}
