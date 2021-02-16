175e46b50449bc4c419a853d1176aae7f0c7c1b9
Mo Moxi <mem53211@gmail.com>
2021-02-16 11:17:23 +0000

    Events

--- Assets/Scripts/Delegate_1.cs
+++ Assets/Scripts/Delegate_1.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Delegate_1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public delegate void ChangeColor(Color newColor);
+
+    public ChangeColor onColorChange;
+
+    public delegate void OnComplete();
+
+    public OnComplete onComplete;
+    
     void Start()
     {
-        
+        onColorChange = UpdateColor;                // signature must match method
+        onColorChange(Color.red);                   // to invoke onColorChange
+        onComplete += Task;                         // signature must match method
+        onComplete += Task;                         // multi delegate use +=. To remove a delegate task -=
+        onComplete();                               // to invoke onComplete    
+    }                                               // note: the delegate can call any method with similar signature    
+
+    public void UpdateColor(Color newColor)
+    {
+        Debug.Log("The colour was changed to..." + newColor.ToString());
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Task()
     {
-        
+Debug.Log("Task Complete");        
+    }
+    
+    // multicast
+    
+    public void Task2()
+    {
+        Debug.Log("This is a multitask delegate result.");        
     }
 }
---
+++ Assets/Scripts/Events_1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Events_1 : MonoBehaviour
+{
+    public delegate void ActionClick();
+
+    public static event ActionClick onClick;
+    
+    public void ButtonClick()
+    {
+        // turn all cubes green
+        if(onClick !=null)
+            onClick();
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
