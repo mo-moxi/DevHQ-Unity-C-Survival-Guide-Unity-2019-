d977d0dfc1719e4b7b63b59fb836c3b80c458588
Mo Moxi <mem53211@gmail.com>
2021-02-16 18:20:22 +0000

    LINQ with Lamda operator

---
+++ Assets/Scripts/Test_Scripts/LINQExample.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class LINQExample : MonoBehaviour
+{
+    public string[] names = {"jon", "gillian", "simon"};
+    // Start is called before the first frame update
+    void Start()
+    {
+    /*    foreach (var item in names)
+        {
+            if(item == "simon")
+                Debug.Log("Name exists: " + name);
+        }
+    */
+    var namesFound = names.Any(name => name == "simon");
+    Debug.Log("Does name exist:" + namesFound);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
