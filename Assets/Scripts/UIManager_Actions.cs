e9c8cd66c0f31eaa2f3deb22e11a4d52e60d32d3
Mo Moxi <mem53211@gmail.com>
2021-02-17 22:00:38 +0000

    Monosingleton

--- Assets/Scripts/UIManager_Actions.cs
+++ Assets/Scripts/UIManager_Actions.cs
@@ -8,7 +8,10 @@ public class UIManager_Actions : MonoBehaviour
     public Text healthText;
     public void OnEnable()
     {
-        Player_Actions.OnDamagedReceived += UpDateHealth;
+        // Standard static Action 
+        // Player_Actions.OnDamagedReceived += UpDateHealth;
+        // Subscription to static instance (singleton) class delegate
+        Player_Actions.Instance.OnDamagedReceived += UpDateHealth;
     }
 
     public void UpDateHealth(int health)
